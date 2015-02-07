bdd55b652ec36fba0bf2634a2070bf833f466aca
Spellchaser <victorkanglei@yahoo.com>
2015-02-07 04:53:29 -0500

    Attempt at using DOS

---
+++ js/blind.js
@@ -0,0 +1,20 @@
+
+function unitSquare(x,y,b,o,v) {
+	this.xCord		= x,
+	this.yCord		= y,
+	this.biome		= b,
+	this.occupied	= o,
+	this.visitedc	= v
+};
+		window.onload = function () {
+			document.getElementById("game-area").innerHTML = 
+			//TODO
+
+		};
+
+		
+function createMatrix() {
+
+
+
+};
