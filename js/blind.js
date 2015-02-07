111adf356e321cc66fd1dcf94afa733972e13b90
Spellchaser <victorkanglei@yahoo.com>
2015-02-07 05:03:11 -0500

    Placeholder Interface

--- js/blind.js
+++ js/blind.js
@@ -1,3 +1,5 @@
+matrix = []
+biome = ["Empty" "Forest" "Lake" "Desert" "Mountain"]
 
 function unitSquare(x,y,b,o,v) {
 	this.xCord		= x,
@@ -6,15 +8,20 @@ function unitSquare(x,y,b,o,v) {
 	this.occupied	= o,
 	this.visitedc	= v
 };
-		window.onload = function () {
-			document.getElementById("game-area").innerHTML = 
+
+
+function randomBiome() {
+	return biome[Math.floor(Math.random()*biome.length)];
+}
+
+	window.onload = function () {
+		for (var row = 0; row < 9; row++) {
+			matrix[row] = [];
+			for (var col = 0; col < 9; col++) {
+				matrix[row][col] = unitSquare(col,row,randomBiome(),false,0);
+			}
+		}
 			//TODO
+			//document.getElementById("game-area").innerHTML = 
 
 		};
-
-		
-function createMatrix() {
-
-
-
-};
